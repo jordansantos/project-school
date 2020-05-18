2b5f5ecf3f9dac7ff6278e8c674c9148d9a8f41f
Jordan Santos <jordansant@gmail.com>
2020-05-18 19:22:33 -0300

    Primeiro commit arquivos iniciais do projeto

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Alunos/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Alunos from './components/Aluno/Alunos'
+
+export default {
+  name: 'App',
+  components: {
+    Alunos
+  }
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+  body { background-color: #eee; font-family: "Montserrat", sans-serif; display: grid; justify-items: center; }
+  body, html { margin: 0; height: 100%; }
+  #app {}
+  table { margin: 0px; padding: 0px; list-style-type: none; width: 100%; }
+  table thead th { background-color: rgb(184, 208, 216) !important; font-size: 1.2em; padding: 10px 0px; text-align: center !important;}
+  table tr, td { padding: 20px; font-size: 1.3em; background-color: #e0edf4; margin-bottom: 2px; color: #3e5252; }
+  .colPequeno { width: 5%; }
+  .btn { background-color: #fa4430; padding: 10px 20px; border: 1px solid #000; cursor: pointer; color: rgb(255, 253, 253); font-weight: bold; border-radius: 5px; border-bottom: 3px solid #000; }
+  .btn:hover { text-shadow: 1px 1px 1px #000; border-bottom: 1px solid #000; margin-top: 3px; }
+</style>
---
+++ frontend/src/components/Aluno/Alunos.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <Titulo texto='Aluno'/>
+    <div>
+      <input type="text" placeholder="Nome do Aluno" required v-model="nome" 
+        v-on:keyup.enter="addAluno()">
+      <button class="btn btnInput" @click="addAluno()">Adicionar</button>
+    </div>
+    <hr>
+    <table border="1px">
+      <thead>
+        <th>Mat.</th>
+        <th>Nome</th>
+        <th>Sobrenome</th>
+        <th>Opções</th>
+      </thead>
+      <tbody v-if="alunos.length">
+        <tr v-for="(aluno, index) in alunos" :key="index">
+          <td>{{ aluno.id }}</td>
+          <td>{{ aluno.nome }}</td>
+          <td>{{ aluno.sobrenome }}</td>
+          <td><button class="btn" @click="remover(aluno)">Remover</button></td>
+        </tr>
+      </tbody>
+      <tfoot v-else>
+        Nenhum Aluno Encontrado
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import Titulo from '../_share/Titulo'
+
+export default {
+  components: {
+    Titulo
+  },
+  data () {
+    return {
+      titulo: 'Aluno',
+      nome: '',
+      alunos: []
+    }
+  },
+  created() {
+    this.$http
+      .get('http://localhost:3000/alunos')
+      .then( res => res.json() )
+      .then( alunos => this.alunos = alunos )
+  },
+  props: {
+  },
+  methods: {
+    addAluno() {
+      let _aluno = {
+        nome: this.nome,
+        sobrenome: ''
+      }
+
+      this.$http
+        .post('http://localhost:3000/alunos', _aluno)
+        .then( res => res.json() )
+        .then( alunoReturn => {
+          this.alunos.push(alunoReturn)
+          this.nome = ''
+        })
+    },
+    remover(aluno) {
+      this.$http
+        .delete(`http://localhost:3000/alunos/${aluno.id}`)
+        .then( () => {
+            let indice = this.alunos.indexOf(aluno)
+            this.alunos.splice(indice, 1)
+          } 
+        )
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  input, .btnInput { border: 0; padding: 20px; font-size: 1.3em; display: inline; }
+  input { color: #687f7f; }
+  .btnInput { background-color: rgb(116, 115, 115);}
+  .btnInput:hover { padding: 20px; margin: 0; border: 0; }
+</style>
